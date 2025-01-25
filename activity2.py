24ee63b46ee111d9546ec828d5f75c70245af3f4
Reda452 <redasakhi0@gmail.com>
2025-01-25 09:51:07 +0100

    new1

---
+++ activity2.py
@@ -0,0 +1,16 @@
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+    result = num1/num2
+    print(f"Result is : {result}")
+
+except ZeroDivisionError:
+    print("Division by zero is not allowed")
+except ValueError:
+    print("Please enter numerical value")
+except NameError as ex:
+    print("the exception is ", ex)
+except:
+    print("Some error occurred")
+finally:
+    print("I will execute no matter what happens")
